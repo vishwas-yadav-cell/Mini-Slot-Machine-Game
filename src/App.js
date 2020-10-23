dcfd1cb04a18bfe5deafd3aebd53d450da28ea35
vishwas-yadav-cell <vishwasyadav164@gmail.com>
2020-10-23 16:19:46 +0530

    'intial'

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Netflix from './components/netflix';
 import Amazon from './components/amazon';
 import Navbar from './components/navbar';
 
-const fav = 'amazon';
+const fav = prompt('What u want amazon/netflix ?');
 
 export default function App() {
     return (
         <>
             <Navbar lgn='login' sgn='Sign in' />
-            {fav === 'netflix' ? <Netflix/> : fav === 'amazon' ? <Amazon/>: <h1>Unable to found:(</h1>}
+            {fav === 'netflix' ? <Netflix /> : fav === 'amazon' ? <Amazon /> : <h1>Unable to found:(</h1>}
         </>
     );
 }
