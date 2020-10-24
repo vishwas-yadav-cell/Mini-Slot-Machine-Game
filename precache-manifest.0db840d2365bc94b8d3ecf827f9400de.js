41719e4be2c25f30dcadfde237bd6d3b27f32827
vishwas-yadav-cell <vishwasyadav164@gmail.com>
2020-10-24 16:40:06 +0530

    Updates

---
+++ precache-manifest.0db840d2365bc94b8d3ecf827f9400de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae565a7e4f1cf6e4ec4284dc07f973f4",
+    "url": "/Mini-Slot-Machine-Game/index.html"
+  },
+  {
+    "revision": "e7e023342b93fc62e108",
+    "url": "/Mini-Slot-Machine-Game/static/css/main.5cdc34ba.chunk.css"
+  },
+  {
+    "revision": "8b5d9402451094fd5f2f",
+    "url": "/Mini-Slot-Machine-Game/static/js/2.02d57afb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Mini-Slot-Machine-Game/static/js/2.02d57afb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e023342b93fc62e108",
+    "url": "/Mini-Slot-Machine-Game/static/js/main.2b45c42a.chunk.js"
+  },
+  {
+    "revision": "0840599645613dc6a712",
+    "url": "/Mini-Slot-Machine-Game/static/js/runtime-main.10a53a76.js"
+  }
+]);
